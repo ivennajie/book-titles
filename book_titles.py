a526898f478e19332caa7fa6f57c40e4834065cc
iven23 <iven23@gmail.com>
2021-12-22 23:01:46 +0700

    New code written

--- book_titles.py
+++ book_titles.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
+
+file = open("books.txt", "r")
+
+titles = file.readlines()
+
+alist = []
+x = 0
+code = ""
+length = 0
+letter =""
+
+for title in titles:
+    alist.append(title.rstrip('\n'))
+    length = len(alist[x])
+    letter = alist[x][0] 
+    code = letter + str(length)
+    x += 1
+    print (code) 
+
+file.close()
 
